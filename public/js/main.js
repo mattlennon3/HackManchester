ab1b441fe77234f8dec85dca39e77145a9f8de99
SWLim <seckwei93@gmail.com>
2015-10-24 20:10:25 +0100

    Merge

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+
 $(".createChallengeButton").click(function(){
     $(".initial").hide();
     $(".connect").show();
@@ -15,3 +16,27 @@ $(".NominateButton").click(function(){
     $(".selectCharity").show();
 });
 
+
+$(document).ready(function(){
+	$('#PickCategory').bind('change', function(){
+		var text = $(this).find("option:selected").text();
+
+		$.ajax({
+			url: 'getChallengesByCat',
+			type: 'POST',
+			data: {
+				category : text
+			},
+			success: function(challenges){
+
+				$('#searchByCatResult').html('');
+
+				for(var ind in challenges){
+					$('#searchByCatResult').append(challenges[ind].Title);
+				}
+			}
+		}).fail(function(err){
+			console.log(err.responseText);
+		});
+	});
+});
