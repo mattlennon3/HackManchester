77b7c8292c6acd47a4e38615d7eaaffc5e2bb494
Gintaras Liksa <gintarasliksa@Gintarass-MacBook-Pro.local>
2015-10-25 10:48:05 +0000

    mine changes

---
+++ resources/views/dashboard/summary.blade.php
@@ -0,0 +1,47 @@
+<div class='summary' style="padding-top: 42px;">
+
+
+<div style="width: 100%;">
+ <div style="float: left; width: 33%;"><div class="row">
+  <div class="col-sm-6 col-md-4" style="width: 80%!important;">
+    <div class="thumbnail">
+      <img src="http://icons.iconarchive.com/icons/gakuseisean/ivista-2/256/Alarm-Arrow-Right-icon.png" height="80" width="80" alt="...">
+      <div class="caption">
+        <h4>Created Challenges</h4>
+        <p></p>
+        <p><a href="#" class="btn btn-primary" role="button"  style="width: 100%!important;">{{ $created }}</a></p>
+      </div>
+    </div>
+  </div>
+</div></div>
+ <div style="float: left; width: 33%;"><div class="row">
+  <div class="col-sm-6 col-md-4" style="width: 80%!important;">
+    <div class="thumbnail">
+      <img src="http://icons.iconarchive.com/icons/gakuseisean/ivista-2/256/Alarm-Arrow-Left-icon.png" height="80" width="80" alt="...">
+      <div class="caption">
+        <h4>Challenges Received</h4>
+        <p></p>
+        <p><a href="#" class="btn btn-primary" role="button" style="width: 100%!important;">{{ $challenged }}</a> </p>
+      </div>
+    </div>
+  </div>
+</div></div>
+ <div style="float: left; width: 33%;"><div class="row">
+  <div class="col-sm-6 col-md-4" style="width: 80%!important;">
+    <div class="thumbnail">
+      <img src="http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/Money-icon.png" height="80" width="80" alt="...">
+      <div class="caption">
+        <h4>Total £ Raised</h4>
+        <p></p>
+        <p><a href="#" class="btn btn-success" role="button" style="width: 100%!important;">£{{ $total }}</a> </p>
+      </div>
+    </div>
+  </div>
+</div></div>
+ <br style="clear: left;" />
+</div>
+</div>
+
+
+
+
--- resources/views/header/nominate-friend.blade.php
+++ resources/views/header/nominate-friend.blade.php
@@ -1,5 +1,17 @@
 
 Select friend you would like to Challenge
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="mattlennon3@hotmail.co.uk">
+<input type="hidden" name="lc" value="GB">
+<input type="hidden" name="item_name" value="Charity Name">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="GBP">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+
 <select class="form-control friendlist" data-style="btn-primary" id="friendlist"> 
   <option value="volvo">Select Friend</option>
 </select>
