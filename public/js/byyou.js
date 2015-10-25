c415bf32399699820b091d65b45b2828f14ac615
Matthew Lennon <mattlennon3@hotmail.co.uk>
2015-10-25 10:38:32 +0000

    haveupushedyesipushed

--- public/js/byyou.js
+++ public/js/byyou.js
@@ -13,16 +13,24 @@ $(document).ready(function(){
 
 					var id = "collapse" + index;
                     var html ='<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#';
-                    html += id + '">' + data[index].Title +'</a><a class="bounty">Bounty:' + data[index].BountyAmount + '</a></h4></div><div id="' + id +'" class="panel-collapse collapse';
+                    html += id + '">' + data[index].Title + '<span style="float:right">Bounty: £'+ data[index].BountyAmount+'</span></a></h4></div><div id="' + id +'" class="panel-collapse collapse';
                     if(index == 0){
                         html+= " in";
                     }
-                    html += '"><div class="panel-body"><p>' + data[index].Description + '</p></div></div></div></div>';
+                    html += '"><div class="panel-body"><p>'+ data[index].Description + '</p><div><span><h4>Category: '+data[index].Category+'  </h4></span><span>Proof Description: '+data[index].ProofDescription+'  </span><span>Proof: '+data[index].ProofLink+'</span></div></div></div></div></div>';
+                    if (data[index].Status == 5){
+
+                    	// code to add button to donate money/accept proof.
+                    	// html += <button id=gimmeyourmoney>
+
+                    }
+
 
                     var $jQueryObject = $.parseHTML(html);
                     //console.log($jQueryObject)
                     $( $jQueryObject ).appendTo( "#accordion" );
-					//console.log(html);
+					//console.log(data[index].BountyAmount);
+
 				});
 
 
