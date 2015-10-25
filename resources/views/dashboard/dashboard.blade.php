dbd7e6ce56aaa92b0543221ddd31ab7c43534367
Matthew Lennon <mattlennon3@hotmail.co.uk>
2015-10-25 04:57:12 +0000

    Dashboard setup with vertical tabs

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,31 +1,36 @@
   @extends('master')
 
 @section('content')
+
+{!! HTML::style('css/bootstrap.vertical-tabs.css') !!}
   <div class="dashboardcontent">
-    <div class="background block_3">
+
+<div class="col-xs-3"> <!-- required for floating -->
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs tabs-left">
+      <li class="active"><a href="#home" data-toggle="tab">Summary</a></li>
+      <li><a href="#profile" data-toggle="tab">For You</a></li>
+      <li><a href="#messages" data-toggle="tab">By you</a></li>
+      <li><a href="#settings" data-toggle="tab">Payments</a></li>
+    </ul>
+</div>
+
+<div class="col-xs-9">
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="home">@include('dashboard.summary')</div>
+      <div class="tab-pane" id="profile">@include('dashboard.for-you')</div>
+      <div class="tab-pane" id="messages">@include('dashboard.by-you')</div>
+      <div class="tab-pane" id="settings">@include('dashboard.account-payment')</div>
     </div>
-    <div class="left_block block_3">
-      <div class="dashboardcontent">
-      		<div class="summaryIcon">
-      		Summary
-      		</div>
-      		<div class="foryouIcon">
-      		For You
-      		</div>
-      		<div class="byyouIcon">
-      		By you
-      		</div>
-      		<div class="paymentsIcon">
-      		Payments
-      		</div>
-      </div>
-    </div>
+</div>  
+<!--
     <div class="background block_5">
     </div>
     <div class="center_block block_5">
       <div class="dashboardcontent">
       @include('dashboard.by-you')
       </div>
-    </div>
+    </div>-->
   </div>
   @stop
