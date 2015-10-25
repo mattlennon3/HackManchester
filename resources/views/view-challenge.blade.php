758d8ef69bf112781ebcf7140197427950e00857
Adam Chappell <adam.chappell@webappuk.com>
2015-10-25 04:06:20 +0000

    Style changes to list changes page

--- resources/views/view-challenge.blade.php
+++ resources/views/view-challenge.blade.php
@@ -1,35 +1,27 @@
 @extends('master')
 
 @section('content')
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-  <form class="navbar-form navbar-left" role="search">
-    <div class ="form-group col-lg-1">
-      <ul class="nav navbar-nav">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" id="dropdownMenu1" aria-expanded="false">Categories<span class="caret"></span></a>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1" >
-		    <li><a class="cat">All</a></li>
-		    <li><a class="cat">Sports</a></li>
-		    <li><a class="cat">Health</a></li>
-		    <li><a class="cat">Trollololol</a></li>
-		    <li><a class="cat">Running</a></li>
-		  </ul>
-		 </li>
-	
-  		</ul>
-  	</div>	
-    <div class="form-group col-lg-1">
-      <input type="text" class="form-control" placeholder="Search for Challenges" style="width: 200px" id="searchtext">
-    
-      <button type="button" id="search" class="btn btn-default">Search</button>
+  <div class="container">
+      <form class="navbar-form navbar-left" role="search">
+          <div class ="form-group">
+                <div class="input-group">
+                    <div class="input-group-btn">
+                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdownMenu1">Categories <span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a class="cat">All</a></li>
+                            <li><a class="cat">Sports</a></li>
+                            <li><a class="cat">Health</a></li>
+                            <li><a class="cat">Trollololol</a></li>
+                            <li><a class="cat">Running</a></li>
+                        </ul>
+                    </div><!-- /btn-group -->
+                    <input type="text" class="form-control" id="searchtext" placeholder="Search for Challenges">
+                </div><!-- /input-group -->   
+                <button type="button" id="search" class="btn btn-default">Search</button>
+            </div>
+        </form>
     </div>
-    </form>    
-      
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+
 <div id="sresults">
 	<div class="panel-group" id="accordion0" style="display: none">
 	    <div class="panel panel-default">
